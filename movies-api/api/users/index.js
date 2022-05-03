1ef6051eb2c8f84e052821edf3671aa6e865d748
AnthonyDoyle6 <20088327@mail.wit.ie>
2022-05-03 15:32:38 +0100

    Topic 10 - JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from './userModel';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -9,22 +10,66 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
-// register(Create)/Authenticate User
-router.post('/', async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
-        });
+// Register OR authenticate a user
+router.post('/',asyncHandler( async (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+      res.status(401).json({success: false, msg: 'Please pass username and password.'});
+      return next();
     }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+    if (req.query.action === 'register') {
+      await User.create(req.body);
+      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+    } else {
+      const user = await User.findByUserName(req.body.username);
+        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (isMatch && !err) {
+            // if user is found and password matches, create a token
+            const token = jwt.sign(user.username, process.env.SECRET);
+            // return the information including token as JSON
+            res.status(200).json({success: true, token: 'BEARER ' + token});
+          } else {
+            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
+          }
+        });
+      }
+  }));
+
+    else {  //NEW CODE!!!
+        const user = await User.findByUserName(req.body.username);
+        if (user.comparePassword(req.body.password)) {
+            req.session.user = req.body.username;
+            req.session.authenticated = true;
+            res.status(200).json({
+                success: true,
+                token: "temporary-token"
+              });
+        } else {
+            res.status(401).json('authentication failed');
         }
+    }
+});
+
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
+    }
+});
+
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
     }
 });
 
